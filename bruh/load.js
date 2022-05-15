19e0f28556430ba03cdafb0daf7cfca221cbd0e8
kartikfire <71930656+kartikfire@users.noreply.github.com>
2022-05-15 19:17:39 +0400

    Update load.js

--- bruh/load.js
+++ bruh/load.js
@@ -6,4 +6,6 @@ frame.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1";
 frame.frameBorder = "0";
 frame.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
 
+setInterval(() => {
 document.getElementById("thing").appendChild(frame);
+}, 5000)
